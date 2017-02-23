76bbe7569103c72a99b0ba1f8414a686ac5de7a8
Yevhen Budachov <y.budachov@levi9.com>
2017-02-23 13:42:19 +0200

    refactorl observables in history componnent

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Http, Response, Headers, RequestOptions } from "@angular/http";
-import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 import { HomeService } from '../../services/home.service';
 import { CompetitionsService } from '../../services/competitions.service';
@@ -31,14 +32,31 @@ export class HistoryComponent implements OnInit {
     highlightTeams = [];
 
     ngOnInit() {
-        this.homeService.getHistory(this.id).subscribe(val => {
-            let data = val.json()
-            this.data = data;
+        this.homeService.getHistory(this.id)
+        .map(this.extractData)
+        .subscribe(this.handleData.bind(this), this.handleError, this.handleComplete);
+    }
+
+    handleData(data){
+        this.data = data;
             this.highlightTeams = [data.fixture._links.homeTeam.id, data.fixture._links.awayTeam.id];
 
             this.competitionsService.getCompetitions(data.fixture._links.competition.id)
-                .map(i => i.json())
+                .map(this.extractData)
                 .subscribe(data => this.table = data.standing);
-        });
+    }
+
+    extractData(response) {
+        const body = response.json();
+        return body || { };
+    }
+
+    handleComplete() {
+        console.log('Complete');
+    }
+
+    handleError(error) {
+        console.log('error:', error)
+        return Observable.throw(error);
     }
 }
