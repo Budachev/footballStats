af27444013b0b9454e21ff2abdd75d89e75afd2e
Zhenya <EvgeniyWide@gmail.com>
2017-05-04 22:50:57 +0300

    added auth guard

--- src/app/modules/record/record.module.ts
+++ src/app/modules/record/record.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { RecordComponent }   from './record.component';
 import { routing } from './record.routing';
+import { AuthGuard } from './config/auth.guard';
 
 @NgModule({
   imports: [routing],
+  providers: [AuthGuard],
   declarations: [RecordComponent]
 })
 export class RecordModule {}
--- src/app/modules/record/record.routing.ts
+++ src/app/modules/record/record.routing.ts
@@ -1,10 +1,11 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './config/auth.guard';
 
 import { RecordComponent } from './record.component';
 
 const routes: Routes = [
-  { path: '', component: RecordComponent }
+  { path: '', component: RecordComponent, canActivate: [AuthGuard] }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
